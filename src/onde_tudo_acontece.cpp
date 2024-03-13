e1a79e5ca29c1f3632d993f7ecdd8cb5e82590fc
Gabriel Lucena <gabriellucenasajr@gmail.com>
2024-03-13 17:44:32 -0300

    iniciado como tudo acontece

--- src/onde_tudo_acontece.cpp
+++ src/onde_tudo_acontece.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 #include "../includes/Aluno.h"
 #include "../includes/Personal.h"
+using namespace std;
 
 int main()
 {
+    int cargo;
 
+    cout << "Você é aluno ou personal? (1 - Personal | 2 - Aluno)" << endl;
+    cin >> cargo;
+
+    while(1)
+    {
+        if (cargo == 1)
+        {
+            Personal personal;
+            Personal.menu();
+
+            break;
+        }
+        else if (cargo == 2)
+        {
+            Aluno aluno;
+            Aluno.menu();
+            
+            break;
+        }
+        else
+        {
+        cout << "Opção inválida!" << endl;
+        }
+    }
 }
-
-
