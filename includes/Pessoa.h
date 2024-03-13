a952e3cabc15392c5092f0032931e1f48ed39339
Gabriel Lucena <gabriellucenasajr@gmail.com>
2024-03-13 15:03:56 -0300

    adicionado pessoa

--- includes/Pessoa.h
+++ includes/Pessoa.h
@@ -1,9 +1,20 @@
+#pragma once
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
 class Pessoa
-  {
-      
-  };
+{
+  public:
+    Pessoa();
+    Pessoa(string nomeCompleto, int i);
+    virtual void exibir() = 0;
+
+    void setNomeCompleto(string n);
+    void setIdade(int i);
+    string getNomeCompleto();
+    int getIdade();
+
+  protected:
+    string nomeCompleto;
+    int idade;
+};
