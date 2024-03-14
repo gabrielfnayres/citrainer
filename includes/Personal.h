8e3d6c0ca1a4812d6a73cbbdea7e61d375c2cdb2
Gabriel Lucena <gabriellucenasajr@gmail.com>
2024-03-14 10:26:57 -0300

    include de vector no personal

--- includes/Personal.h
+++ includes/Personal.h
@@ -1,24 +1,26 @@
 #include <iostream>
+#include <vector>
 #include "Aluno.h"
 using namespace std;
 
 class Personal
   {
   private:
-    vector<Aluno> aluno(100);
-  public:
-    Personal();
-    Personal(int trainerID, Aluno aluno[]);
-    
-    Aluno getAluno();
-    
-    void setAluno(Aluno aluno);
+      vector<Aluno> aluno = vector<Aluno>(100);
 
-    void cadastrarAluno();
-    void alterarAluno();
-    void removerAluno();
-    void verAluno();
-    void exibir();
-    void gerarID();
-    void menu();
+    public:
+      Personal();
+      Personal(int trainerID, Aluno aluno[]);
+      
+      Aluno getAluno();
+      
+      void setAluno(Aluno aluno);
+
+      void cadastrarAluno();
+      void alterarAluno();
+      void removerAluno();
+      void verAluno();
+      void exibir();
+      void gerarID();
+       void menu();
   };
