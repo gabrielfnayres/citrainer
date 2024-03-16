b5ccbc9d7a796a50e41e14ae3441f2933386926a
Adriane Lopes <adrianelf04@gmail.com>
2024-03-15 21:23:36 -0300

    classe endereco

---
+++ includes/Endereco.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Endereco{
+protected:
+string bairro;
+string cidade;
+string CEP;
+string rua;
+int numero;
+
+public:
+Endereco();
+void setBairro(string b);
+void setCidade(string c);
+void setCep(string cep);
+void setRua(string r);
+void setNumero(int n);
+string getBairro();
+string getCidade();
+string getCep();
+string getRua();
+int getNumero();
+void exibirEndereco();
+}
---
+++ src/Endereco.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Endereco.h"
+
+
+Endereco::Endereco(){
+
+}
+
+Endereco::Endereco(string b, string c, string r, string cep, int n){
+    
+}
