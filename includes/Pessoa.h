b85e32414a4a8680e45024b63940e78e2dbcb34e
Gabriel Lucena <gabriellucenasajr@gmail.com>
2024-03-15 21:49:08 -0300

    resolvido bugs em pessoa

--- includes/Pessoa.h
+++ includes/Pessoa.h
@@ -1,23 +1,27 @@
 #pragma once
 #include <iostream>
+#include "Endereco.h"
 using namespace std;
 
 class Pessoa
 {
   public:
     Pessoa();
-    Pessoa(string nomeCompleto, int i, string data);
+    Pessoa(string nomeCompleto, int idade, string data, Endereco endereco);
     virtual void exibir() = 0;
 
     void setNomeCompleto(string n);
     void setIdade(int i);
+    void setDataDeNascimento(string d);
+    void setEndereco(Endereco e);
     string getNomeCompleto();
     int getIdade();
-    void setDataDeNascimento(string data);
     string getDataDeNascimento();
+    Endereco getEndereco();
 
   protected:
     string nomeCompleto;
     int idade;
     string dataDeNascimento;
+    Endereco endereco;
 };
