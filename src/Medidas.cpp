6d2cf4e87971184c69a9597f198d3e3d316678ff
Gabriel Lucena <gabriellucenasajr@gmail.com>
2024-03-18 13:52:20 -0300

    sei lá, muita coisa mas a maioria besteira

--- src/Medidas.cpp
+++ src/Medidas.cpp
@@ -121,7 +121,7 @@ string Medidas::toStringMedidas(){
      co << fixed << setprecision(2) << coxa;
      pa << fixed << setprecision(2) << panturrilha;
     
-    stmedidas = "---------- Medidas -----------\nPeso(kg): " + p.str() + "\n" + "Altura(m): " + a.str() + "\n" + "Cintura(cm): " + c.str() + "\n" + "Busto(cm): " + b.str() + "\n " + "Quadril(cm): " + q.str() + "\n" + "Coxa(cm): " + co.str() + "\n" + "Panturrilha(cm): " + pa.str();
+    stmedidas = "---------- Medidas -----------\nPeso(kg): " + p.str() + "\n" + "Altura(m): " + a.str() + "\n" + "Cintura(cm): " + c.str() + "\n" + "Busto(cm): " + b.str() + "\n" + "Quadril(cm): " + q.str() + "\n" + "Coxa(cm): " + co.str() + "\n" + "Panturrilha(cm): " + pa.str();
 
     return stmedidas;
 }
